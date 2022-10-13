bb42a878879b546550e28c98671c87a38a083a00
Polina Tyureva <polina.tyureva@devexpress.com>
2022-10-13 15:50:26 +0400

    add cs solution

---
+++ CS/AspNetCoreDashboard/Code/DashboardUtils.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.DashboardAspNetCore;
+using DevExpress.DashboardCommon;
+using DevExpress.DashboardWeb;
+using DevExpress.DataAccess.Excel;
+using DevExpress.DataAccess.Sql;
+using Microsoft.Extensions.FileProviders;
+
+namespace AspNetCoreDashboard {
+    public static class DashboardUtils {
+        public static DashboardConfigurator CreateDashboardConfigurator(IConfiguration configuration, IFileProvider fileProvider) {
+            DashboardConfigurator configurator = new DashboardConfigurator();
+            configurator.SetConnectionStringsProvider(new DashboardConnectionStringsProvider(configuration));
+
+            DashboardFileStorage dashboardFileStorage = new DashboardFileStorage(fileProvider.GetFileInfo("Data/Dashboards").PhysicalPath);
+            configurator.SetDashboardStorage(dashboardFileStorage);
+
+            DataSourceInMemoryStorage dataSourceStorage = new DataSourceInMemoryStorage();
+
+            return configurator;
+        }
+    }
+}
---
+++ CS/AspNetCoreDashboard/wwwroot/js/ParameterValues.js
@@ -0,0 +1,38 @@
+﻿function onBeforeRender(sender) {
+    var dashboardControl = sender;
+
+    if (dashboardControl) {
+        dashboardControl.on('dashboardEndUpdate', function () { setParameterValues(dashboardControl); })
+    }
+}
+
+function setParameterValues(control) {
+    var parameterDialogExt = control.findExtension('dashboardParameterDialog');
+
+    $("#setParameterValuesButton").dxButton({
+        text: 'Specify Parameter Values',
+        onClick: function () {
+            var parameters = parameterDialogExt.getParameters();
+            var paramCategory = parameters.getParameterByName("categoryParameter"),
+                paramStartDate = parameters.getParameterByName("startDateParameter");
+            paramCategory.setValue("Condiments");
+            paramStartDate.setValue(new Date(2015, 3, 1));
+        }
+    });
+    $("#resetParameterValuesButton").dxButton({
+        text: 'Reset Parameter Values',
+        onClick: function () {
+            var parameters = parameterDialogExt.getParameters();
+            var paramCategory = parameters.getParameterByName("categoryParameter"),
+                paramStartDate = parameters.getParameterByName("startDateParameter");
+            paramCategory.setValue(paramCategory.getDefaultValue());
+            paramStartDate.setValue(paramStartDate.getDefaultValue());
+        }
+    });
+    $("#showParametersDialog").dxButton({
+        text: 'Show Parameters Dialog',
+        onClick: function () {
+            parameterDialogExt.show();
+        }
+    });
+}
